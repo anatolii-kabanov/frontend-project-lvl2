373900fe5ddf313805701affa930548e40a9936d
Anatolii Kabanov <4akabanov@gmail.com>
2020-12-05 00:20:30 +0100

    Second step.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ import { Command } from 'commander';
 
 const program = new Command();
 program
+    .arguments('<filepath1> <filepath2>')
     .version('1.0.0', '-v, --version', 'output the version number')
     .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
     .parse(process.argv);
