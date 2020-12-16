9309e31937dea2aa249db7b00d8caa5d9f597a0f
Anatolii Kabanov <4akabanov@gmail.com>
2020-12-16 20:12:10 +0100

    Removed back slashes from stylish, test for plain formatter.

--- __tests__/formatters/plain.formatter.test.js
+++ __tests__/formatters/plain.formatter.test.js
@@ -10,3 +10,8 @@ test('Plain Formatter JSON should return formatted value', () => {
   const resultJson = JSON.parse(readFile('complex-result.json'));
   expect(plainFormatter(resultJson)).toBeDefined();
 });
+
+test('Plain Formatter JSON should contain expected row', () => {
+  const resultJson = JSON.parse(readFile('complex-result.json'));
+  expect(plainFormatter(resultJson)).toContain("Property 'common.setting5' was added with value: [complex value]");
+});
--- src/stylish.js
+++ src/stylish.js
@@ -1,5 +1,5 @@
 export default function stylish(json) {
   let result = '';
-  result = JSON.stringify(json, null, 2).replace(/"/g, '').replace(/'/g, '');
+  result = JSON.stringify(json, null, 2).replace(/"/g, '').replace(/'/g, '').replace(/\\/g, '');
   return result;
 }
