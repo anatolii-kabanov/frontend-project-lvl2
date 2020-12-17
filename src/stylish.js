52baedb593436cadd6ebbf1aab35b2041bd47353
Anatolii Kabanov <4akabanov@gmail.com>
2020-12-17 13:13:06 +0100

    Formatted stylish js.

--- src/stylish.js
+++ src/stylish.js
@@ -1,5 +1,3 @@
 export default function stylish(json) {
-  let result = '';
-  result = JSON.stringify(json, null, 2).replace(/"/g, '').replace(/'/g, '').replace(/\\/g, '');
-  return result;
+  return JSON.stringify(json, null, 2).replace(/"/g, '').replace(/'/g, '').replace(/\\/g, '');
 }
