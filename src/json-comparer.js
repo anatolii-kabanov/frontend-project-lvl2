555e0acba3048c29f67c43070a65f05fc6e13528
Anatolii Kabanov <4akabanov@gmail.com>
2020-12-17 13:16:47 +0100

    Formatted json comparer js.

--- src/json-comparer.js
+++ src/json-comparer.js
@@ -2,35 +2,27 @@ import _ from 'lodash';
 
 export default function getDifference(firstObject, secondObject) {
   const secondObj = _.cloneDeep(secondObject);
-  const diff = _.reduce(
-    firstObject,
-    (result, value, key) => {
-      const resultObj = _.cloneDeep(result);
-      if (_.has(secondObj, key)) {
-        if (_.isEqual(firstObject[key], secondObj[key])) {
-          resultObj[key] = value;
-        } else if (typeof (value) === 'object' && typeof (secondObj[key]) === 'object') {
-          resultObj[key] = getDifference(value, secondObj[key]);
-        } else {
-          resultObj[`- ${key}`] = value;
-          resultObj[`+ ${key}`] = secondObj[key];
-        }
-        delete secondObj[key];
+  const diff = _.reduce(firstObject, (result, value, key) => {
+    const resultObj = _.cloneDeep(result);
+    if (_.has(secondObj, key)) {
+      if (_.isEqual(firstObject[key], secondObj[key])) {
+        resultObj[key] = value;
+      } else if (typeof (value) === 'object' && typeof (secondObj[key]) === 'object') {
+        resultObj[key] = getDifference(value, secondObj[key]);
       } else {
         resultObj[`- ${key}`] = value;
+        resultObj[`+ ${key}`] = secondObj[key];
       }
-      return resultObj;
-    },
-    {},
-  );
-  const resultObject = _.reduce(
-    secondObj,
-    (result, value, key) => {
-      const resultObj = _.cloneDeep(result);
-      resultObj[`+ ${key}`] = value;
-      return resultObj;
-    },
-    diff,
-  );
+      delete secondObj[key];
+    } else {
+      resultObj[`- ${key}`] = value;
+    }
+    return resultObj;
+  }, {});
+  const resultObject = _.reduce(secondObj, (result, value, key) => {
+    const resultObj = _.cloneDeep(result);
+    resultObj[`+ ${key}`] = value;
+    return resultObj;
+  }, diff);
   return resultObject;
 }
